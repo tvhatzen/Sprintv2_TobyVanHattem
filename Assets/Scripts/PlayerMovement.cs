a3e0abcc969c4ef5151bba7a02ac2029a05a516c
tvhatzen <w0507167@nscc.ca>
2024-10-10 01:39:46 -0300

    Camera and Movement Re-Do
    I am following tutorials for this because I am not built for coding and the fact that its 1:30 am does not help

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    public Transform orientation;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
